cf4432ad0496211cdcbff5dafbb13d2d3050f919
Michael Ottink <ottinkmike@gmail.com>
2020-07-13 01:30:09 +0200

    Added text utills import

--- android/src/main/java/in/jvapps/system_alert_window/SystemAlertWindowPlugin.java
+++ android/src/main/java/in/jvapps/system_alert_window/SystemAlertWindowPlugin.java
@@ -12,6 +12,7 @@ import android.os.Build;
 import android.provider.Settings;
 import android.util.Log;
 import android.widget.Toast;
+import android.text.TextUtils;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
